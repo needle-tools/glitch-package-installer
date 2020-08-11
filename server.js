c008de7f31db203c105c92d29ae28af8e2182b7e
Glitch (hello-express) <none>
2020-08-11 19:55:51 +0000

    👚🦀 Checkpoint ./.glitch-assets:4714662/363 ./package.json:4714662/1327 ./server.js:4714662/1858 ./archtemp.json:4714662/276234

--- server.js
+++ server.js
@@ -28,6 +28,42 @@ app.get("/dreams", (request, response) => {
   response.json(dreams);
 });
 
+const targz = require('targz');
+ 
+// compress files into tar.gz archive
+targz.compress({
+    src: 'path_to_files',
+    dest: 'path_to_compressed_file'
+}, function(err){
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Done!");
+    }
+});
+ 
+// decompress files from tar.gz archive
+targz.decompress({
+    src: 'path_to_compressed file',
+    dest: 'path_to_extract'
+}, function(err){
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Done!");
+    }
+});
+
+// https://stackoverflow.com/questions/41941724/nodejs-sendfile-with-file-name-in-download
+// send the .unitypackage back
+app.get("/package", (request, response) => {
+  // express helps us take JS objects and send them as JSON
+  // response.sendFile('archtemp.unitypackage', { root: __dirname });
+  response.download(__dirname + "/" + "archtemp.unitypackage", "my_package.unitypackage");
+  
+  // response.sendFile("https://cdn.glitch.com/ea155149-e3d1-4828-bc6a-2e46ba8cf214%2Farchtemp.unitypackage?v=1597175110752");
+});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
